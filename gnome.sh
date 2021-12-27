9abdcd208e045c809985e633e402ba8c3e26a6fb
Eliliyah <94532192+Eliliyah@users.noreply.github.com>
2021-12-27 10:57:36 -0500

    Update gnome.sh

--- gnome.sh
+++ gnome.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
-pacman -S networkmanager dhclient pacman-contrib curl reflector rsync --needed --noconfirm
+pacman -S networkmanager dhcpcd dhclient pacman-contrib curl rsync --needed --noconfirm
 systemctl enable --now NetworkManager
-pacman -S linux-zen linux-lts 
-pacman -S git wayland xorg-xwayland gdm wayland-protocols qt5-wayland libva xorg xdg-user-dirs gnome eog nemo fish opera dhcpcd
+pacman -S linux-zen linux-lts --noconfirm
+pacman -S git wayland xorg-xwayland gnome-shell gdm wayland-protocols qt5-wayland libva xorg xdg-user-dirs eog nemo fish opera 
