6925ccf44d7e9f7a62dd6f061ba1477797cf45f8
Eliliyah <94532192+Eliliyah@users.noreply.github.com>
2021-12-27 06:07:59 -0500

    Update mount.sh

--- mount.sh
+++ mount.sh
@@ -5,5 +5,4 @@ swapon -L swap
 mount -t btrfs -o subvol=root,$o_btrfs LABEL=system /mnt
 mount -t btrfs -o subvol=home,$o_btrfs LABEL=system /mnt/home
 mount -t btrfs -o subvol=snapshots,$o_btrfs LABEL=system /mnt/.snapshots
-mkdir /mnt/boot
 mount LABEL=EFI /mnt/boot
