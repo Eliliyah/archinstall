9f10542ad9456ecc961d7457dee2768c888a9075
Eliliyah <94532192+Eliliyah@users.noreply.github.com>
2021-12-27 04:47:51 -0500

    Update crypt.sh

--- crypt.sh
+++ crypt.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-mkfs.fat -F32 -n EFI /dev/sda1
+mkfs.fat -F32 -n EFI /dev/sda1/EFI
 
 cryptsetup open --type plain --key-file /dev/urandom /dev/sda2 swap
 mkswap -L swap /dev/mapper/swap
@@ -20,4 +20,4 @@ mount -t btrfs -o subvol=root,$o_btrfs LABEL=system /mnt
 mount -t btrfs -o subvol=home,$o_btrfs LABEL=system /mnt/home
 mount -t btrfs -o subvol=snapshots,$o_btrfs LABEL=system /mnt/.snapshots
 mkdir /mnt/boot
-mount /dev/sda1 /mnt/boot
+mount LABEL=EFI /mnt/boot
