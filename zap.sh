cd8df4aaef647b843ea42cdff7d34dba66ff7fec
Eliliyah <94532192+Eliliyah@users.noreply.github.com>
2021-12-27 09:39:27 -0500

    Delete zap.sh

--- zap.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/bash
-sgdisk --zap-all /dev/sda
-
-sgdisk --clear \
-         --new=1:0:+550MiB --typecode=1:ef00 --change-name=1:EFI \
-         --new=2:0:+1GiB   --typecode=2:8200 --change-name=2:swap \
-         --new=3:0:0       --typecode=3:8300 --change-name=3:cryptsystem \
-           /dev/sda
