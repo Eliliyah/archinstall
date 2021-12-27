ff8cc06e3be6b5cd3a6e71cb47f2f54af86a99d5
Eliliyah <94532192+Eliliyah@users.noreply.github.com>
2021-12-27 09:39:35 -0500

    Delete mount.sh

--- mount.sh
+++
@@ -1,8 +0,0 @@
-cryptsetup open --type plain --key-file /dev/urandom /dev/sda2 swap
-mkswap -L swap /dev/mapper/swap
-swapon -L swap
-
-mount -t btrfs -o subvol=root,$o_btrfs LABEL=system /mnt
-mount -t btrfs -o subvol=home,$o_btrfs LABEL=system /mnt/home
-mount -t btrfs -o subvol=snapshots,$o_btrfs LABEL=system /mnt/.snapshots
-mount LABEL=EFI /mnt/boot
