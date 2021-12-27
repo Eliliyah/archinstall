b2cfd116a19fc259853244308090a6bec9dc31c2
Eliliyah <94532192+Eliliyah@users.noreply.github.com>
2021-12-27 02:02:35 -0500

    Add files via upload

---
+++ crypt.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/bash
+
+mkfs.fat -F32 -n EFI /dev/sda1
+
+cryptsetup open --type plain --key-file /dev/urandom /dev/sda2 swap
+mkswap -L swap /dev/mapper/swap
+swapon -L swap
+
+mkfs.btrfs --force --label system /dev/mapper/system
+o=defaults,x-mount.mkdir
+o_btrfs=$o,compress=lzo,ssd,noatime
+mount -t btrfs LABEL=system /mnt
+
+btrfs subvolume create /mnt/root
+btrfs subvolume create /mnt/home
+btrfs subvolume create /mnt/snapshots
+umount -R /mnt
+
+mount -t btrfs -o subvol=root,$o_btrfs LABEL=system /mnt
+mount -t btrfs -o subvol=home,$o_btrfs LABEL=system /mnt/home
+mount -t btrfs -o subvol=snapshots,$o_btrfs LABEL=system /mnt/.snapshots
+mkdir /mnt/boot
+mount /dev/sda1 /mnt/boot
---
+++ pacstrap.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/bash
+pacstrap /mnt base linux linux-firmware
+pacstrap /mnt dhcpcd linux-zen base-devel btrfs-progs iw gptfdisk zsh terminus-font intel-ucode snapper grub dosfstools man-db man-pages nano usbutils which neofetch fish efibootmgr reflector perl perl-timedate iwd git systemd 
