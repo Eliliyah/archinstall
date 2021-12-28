ec47cc85208e3707192296a5f493c198ff380b04
Eliliyah <94532192+Eliliyah@users.noreply.github.com>
2021-12-27 23:57:03 -0500

    Delete gnome.sh

--- gnome.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/bash
-pacman -S networkmanager dhcpcd dhclient pacman-contrib curl rsync --needed --noconfirm
-systemctl enable --now NetworkManager
-pacman -S linux-zen linux-lts --noconfirm
-pacman -S git wayland xorg-xwayland gnome-shell gdm wayland-protocols qt5-wayland libva xorg xdg-user-dirs eog nemo fish opera 
