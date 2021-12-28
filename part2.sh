45b8e859d34f8ca975677602349001330f69692c
Eliliyah <94532192+Eliliyah@users.noreply.github.com>
2021-12-28 01:58:06 -0500

    Update part2.sh

--- part2.sh
+++ part2.sh
@@ -18,13 +18,17 @@ example-function() {
 
 confirm "Did you check what you're supposed to check?" $(echo "Excellent. You haven't broken it. Yet.")
 
-useradd -m -G wheel -s /bin/bash ellie
-passwd ellie
-
+#Set the root password
+passwd
 confirm "Did you check what you're supposed to check?" $(echo "Excellent. You haven't broken it. Yet.")
 
-
+#add yourself as a user
+useradd -m -G wheel -s /bin/bash ellie
+passwd ellie
 echo "ellie ALL=(ALL)ALL">> /etc/sudoers
+confirm "Did you check what you're supposed to check?" $(echo "Excellent. You haven't broken it. Yet.")
+
+#Copy files
 mv /etc/hostname /etc/backup/hostname 
 cp /archinstall/hostname /etc/hostname
 mv /etc/locale.gen /etc/backup/
@@ -40,24 +44,33 @@ cp /archinstall/blackarch-mirrorlist /etc/pacman.d/blackarch-mirrorlist
 cp /archinstall/vconsole.conf /etc/vconsole.conf
 mv /etc/mkinitcpio.conf /etc/backup
 cp /archinstall/mkinitcpio.conf /etc/mkinitcpio.conf
-
 ls /etc/backup
-
 confirm "Did you check what you're supposed to check?" $(echo "Excellent. You haven't broken it. Yet.")
 
+#Enable system services
 systemctl enable NetworkManager
 systemctl enable systemd-timesyncd.service
 systemctl enable gdm.service
 systemctl enable dhcpcd
 timedatectl status 
-
 confirm "Did you check what you're supposed to check?" $(echo "Excellent. You haven't broken it. Yet.")
 
+#Run mkinitcpio 
 mkinitcpio -p linux
+confirm "Did you check what you're supposed to check?" $(echo "Excellent. You haven't broken it. Yet.")
+
+#Install grub
 grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
-
 confirm "Did you check what you're supposed to check?" $(echo "Excellent. You haven't broken it. Yet.")
 
+#Exit the chroot
 exit
+confirm "Did you check what you're supposed to check?" $(echo "Excellent. You haven't broken it. Yet.")
+
+#Unmount the drive
 umount -R /mnt
+confirm "Did you check what you're supposed to check?" $(echo "Excellent. You haven't broken it. Yet.")
+
+#Reboot
+reboot
