f28b8656c4a4794d4a2f22a32b04b3037ca5c823
Eliliyah <94532192+Eliliyah@users.noreply.github.com>
2021-12-28 06:01:13 -0500

    Update postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -10,13 +10,16 @@ confirm() {
             [Nn]* ) echo "aborted"; exit;;
             * ) echo "Please answer yes or no.";;
         esac
-    done
-}
-example-function() {
-    echo "Excellent. You haven't broken it. Yet."
-}
+    done }
+
+example-function() { echo "Excellent. You haven't broken it. Yet." }
 
 confirm "Are you ready to keep going?" $(echo "Say your prayers.")
+
+sudo Pacman -Syu 
+sudo pacman -S flatpak libappimage wget yajl
+
+sudo pacman -S nemo fish opera meld file-roller opera-ffmpeg-codecs bitwarden code inkscape libreoffice-fresh virtualbox qbittorrent strawberry thunderbird totem bpytop firefox cryptsetup gimp pam-u2f rclone gparted  
 
 cd /tmp
 git clone https://aur.archlinux.org/package-query.git
@@ -26,8 +29,9 @@ git clone https://aur.archlinux.org/yaourt.git
 cd yaourt/
 makepkg -si
 confirm "Did yaourt install successfully?" $(echo "Why does that always make me want yogurt?")
-yaourt -S pamac-aur
-confirm "Did pamac install?" $(echo "Okay, but you know that's the lazy way of doing things.")
+yaourt -S pamac-all
+yaourt -S snapd
+yaourt -S timeshift
 cd /tmp 
 git clone https://aur.archlinux.org/yay.git
 cd yay
