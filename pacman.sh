9806929f449939e6eb8992ce133eb03d631823c2
Eliliyah <94532192+Eliliyah@users.noreply.github.com>
2021-12-28 05:33:42 -0500

    Update pacman.sh

--- pacman.sh
+++ pacman.sh
@@ -6,6 +6,6 @@ pacman -S gdm gnome-shell
 
 pacman -S gnome-terminal --needed gnome-desktop cinnamon-desktop gnome-control-center gnome-system-monitor gnome-tweaks  gnome-color-manager gnome-usage gnome-screenshot gnome-keyring gnome-nettool gnome-calculator 
 
-pacman -S flatpak wget nemo fish payucontrol opera meld file-roller opera-ffmpeg-codecs
+pacman -S snapd flatpak libappimage wget yajl
 
-pacman -S bitwarden code inkscape libreoffice-fresh virtualbox qbittorrent strawberry thunderbird totem bpytop chrome-gnome-shell firefox cryptsetup gimp  pam-u2f rclone gparted yajl libappimage 
+pacman -S nemo fish payucontrol opera meld file-roller opera-ffmpeg-codecs bitwarden code inkscape libreoffice-fresh virtualbox qbittorrent strawberry thunderbird totem bpytop chrome-gnome-shell firefox cryptsetup gimp pam-u2f rclone gparted  
