a464df73d7b9ce5db140705c919e595eb340de0c
Eliliyah <94532192+Eliliyah@users.noreply.github.com>
2021-12-29 06:53:18 -0500

    Update postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -2,21 +2,25 @@
 
 sudo pacman -Syu 
 sudo pacman -S flatpak libappimage wget yajl
-
 sudo pacman -S nemo fish opera gimp discord meld file-roller opera-ffmpeg-codecs bitwarden code inkscape virtualbox qbittorrent strawberry thunderbird totem bpytop firefox cryptsetup gimp pam-u2f rclone gparted 
 
-cd /tmp
-git clone https://aur.archlinux.org/package-query.git
-cd package-query/
-makepkg -si && cd /tmp/
-git clone https://aur.archlinux.org/yaourt.git
-cd yaourt/
-makepkg -si
+flatpak install com.wps.Office
+flatpak install org.gnome.clocks  
+flatpak install io.mgba.mgba
+flatpak install io.github.dosbox-staging
 
-cd /tmp 
+cd /tmp
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si 
+yay -Y --devel --save
+
+yay -S pamac-all --noconfirm
+yay -S timeshift timeshift-autosnap btrfs-autosnap --noconfirm
+yay -S chrome-gnome-shell debtap loginized masterpdfeditor-free mullvad-vpn virtualbox-ext-oracle --noconfirm
+yay -S appimagelauncher --noconfirm
+yay -S icedrive-appimage --noconfirm
+yay gimp-plugin-gmic gimp-palletes-davidrevoy gimp-plugin-akkana-git gimp-plugin-astronomy gimp-plugin-beautify gimp-plugin-contrastfix gimp-plugin-create-layer-mask-from gimp-plugin-duplicate-to-another-image gimp-plugin-export-layers gimp-plugin-image-reg gimp-plugin-instagram-effects gimp-plugin-layerfx gimp-plugin-layer-via-copy-cut gimp-plugin-pandora gimp-plugin-place-layer-into-selection gimp-plugin-registry gimp-plugin-scale-layer-to-image-size gimp-plugin-toy gimp-plugin-refocus --noconfirm
 
 cd/tmp
 git clone https://github.com/safing/portmaster-packaging
@@ -24,44 +28,3 @@ cd portmaster-packaging/linux
 makepkg -is
 
 systemctl enable portmaster
-
-flatpak install com.wps.Office
-flatpak install org.gnome.clocks  
-flatpak install io.mgba.mgba
-flatpak install io.github.dosbox-staging
-
-yaourt -S pamac-all
-yaourt -S snapd
-yaourt -S timeshift
-yaourt -S timeshift-autosnap
-yaourt -S btrfs-autosnap
-yaourt -S chrome-gnome-shell
-yaourt -S debtap
-yaourt -S loginized
-yaourt -S masterpdfeditor-free
-yaourt -S mullvad-vpn
-yaourt -S virtualbox-ext-oracle
-yaourt -S gimp-plugin-gmic
-yaourt -S gimp-palletes-davidrevoy
-yaourt -S gimp-plugin-akkana-git
-yaourt -S gimp-plugin-astronomy 
-yaourt -S gimp-plugin-beautify
-yaourt -S gimp-plugin-contrastfix
-yaourt -S gimp-plugin-create-layer-mask-from
-yaourt -S gimp-plugin-duplicate-to-another-image
-yaourt -S gimp-plugin-export-layers
-yaourt -S gimp-plugin-image-reg
-yaourt -S gimp-plugin-instagram-effects
-yaourt -S gimp-plugin-layerfx
-yaourt -S gimp-plugin-layer-via-copy-cut
-yaourt -S gimp-plugin-pandora
-yaourt -S gimp-plugin-place-layer-into-selection
-yaourt -S gimp-plugin-registry
-yaourt -S gimp-plugin-scale-layer-to-image-size
-yaourt -S gimp-plugin-toy
-yaourt -S gimp-plugin-refocus
-
-yay -S appimagelauncher
-yay -S icedrive-appimage
-
-timedatectl status 
