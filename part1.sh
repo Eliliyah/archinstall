9e81fd486a7830ddec5ac1418617cfeb4a94a6fb
Eliliyah <94532192+Eliliyah@users.noreply.github.com>
2021-12-29 06:52:36 -0500

    Update part1.sh

--- part1.sh
+++ part1.sh
@@ -23,26 +23,36 @@ loadkeys us
 timedatectl set-timezone America/New_York
 timedatectl set-ntp true
 
-#Partition the drive
+#Format the drive
 sgdisk --zap-all /dev/sda
 
-confirm "Did it zap?" 
-
 sgdisk --clear \
-         --new=1:0:+550MiB --typecode=1:ef00 --change-name=1:EFI \
-         --new=2:0:+1GiB   --typecode=2:8200 --change-name=2:swap \
-         --new=3:0:0       --typecode=3:8300 --change-name=3:system \
+         --new=1:0:+550MiB --typecode=1:ef00 \
+         --new=2:0:+1GiB   --typecode=2:8200 \
+         --new=3:0:0       --typecode=3:8300 \
            /dev/sda
 
-#Format the new partitions
-mkfs.fat -F 32 /dev/sda1
-mkswap /dev/sda2
-swapon /dev/sda2
-mkfs.btrfs -f /dev/sda3
-mount /dev/sda3 /mnt
+#Partition the drive and create subvolumes
+mkfs.fat -F 32 -n EFI /dev/sda1 
+mkswap -L swap -f /dev/sda2 
+mkfs.btrfs /dev/sda3 --label=system -f
+o=defaults,x-mount.mkdir
+o_btrfs=$o
+mount -t btrfs LABEL=system /mnt 
+btrfs subvolume create /mnt/root
+btrfs subvolume create /mnt/home
+btrfs subvolume create /mnt/snapshots
 mkdir /mnt/boot
+umount -R /mnt
+
+#Mount the partitions
+mount -t btrfs -o subvol=root,$o_btrfs LABEL=system /mnt
+mount -t btrfs -o subvol=home,$o_btrfs LABEL=system /mnt/home
+mount -t btrfs -o subvol=snapshots,$o_btrfs LABEL=system /mnt/snapshots
+mkfs.fat -F 32 -n EFI /dev/sda1 
 mount /dev/sda1 /mnt/boot
-fdisk -l 
+swapon -L swap
+lsblk
 confirm "Partitions look okay?" 
 
 #Install the base system
@@ -55,6 +65,5 @@ genfstab -U /mnt >> /mnt/etc/fstab
 confirm "Ready to chroot?" 
 
 #Prepare the next steps
-chmod +x copy.sh
-chmod+x part2.sh
+chmod +x part2.sh
 timedatectl status
