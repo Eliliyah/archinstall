24ca75e8bd1afbf3c385887b71eaa30346d05e7b
Eliliyah <94532192+Eliliyah@users.noreply.github.com>
2021-12-29 08:00:49 -0500

    Update part1.sh

--- part1.sh
+++ part1.sh
@@ -42,13 +42,13 @@ mount -t btrfs LABEL=system /mnt
 btrfs subvolume create /mnt/root
 btrfs subvolume create /mnt/home
 btrfs subvolume create /mnt/snapshots
-mkdir /mnt/boot
 umount -R /mnt
 
 #Mount the partitions
 mount -t btrfs -o subvol=root,$o_btrfs LABEL=system /mnt
 mount -t btrfs -o subvol=home,$o_btrfs LABEL=system /mnt/home
 mount -t btrfs -o subvol=snapshots,$o_btrfs LABEL=system /mnt/snapshots
+mkdir /mnt/boot
 mkfs.fat -F 32 -n EFI /dev/sda1 
 mount /dev/sda1 /mnt/boot
 swapon -L swap
@@ -60,10 +60,8 @@ pacstrap /mnt base linux linux-firmware
 pacstrap /mnt dhcpcd linux-zen base-devel btrfs-progs iw gptfdisk zsh terminus-font intel-ucode snapper grub dosfstools man-db man-pages nano usbutils util-linux which neofetch python efibootmgr efitools efivar reflector perl perl-timedate iwd git systemd grub-btrfs xorg xdg-user-dirs 
 confirm "Was install successful?" 
 
-#Generate fstab
-genfstab -L -p /mnt >> /mnt/etc/fstab
-confirm "Ready to chroot?" 
-
 #Prepare the next steps
 chmod +x part2.sh
 timedatectl status
+genfstab -L -p /mnt >> /mnt/etc/fstab
+nano /mnt/etc/fstab
