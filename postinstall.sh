9b8fd4d298652da352350c0a3941654fb85e9b69
Eliliyah <94532192+Eliliyah@users.noreply.github.com>
2021-12-29 00:13:49 -0500

    Update postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -3,7 +3,7 @@
 sudo Pacman -Syu 
 sudo pacman -S flatpak libappimage wget yajl
 
-sudo pacman -S nemo fish opera meld file-roller opera-ffmpeg-codecs bitwarden code inkscape libreoffice-fresh virtualbox qbittorrent strawberry thunderbird totem bpytop firefox cryptsetup gimp pam-u2f rclone gparted gimp-plugin-gmic 
+sudo pacman -S nemo fish opera gimp discord meld file-roller opera-ffmpeg-codecs bitwarden code inkscape virtualbox qbittorrent strawberry thunderbird totem bpytop firefox cryptsetup gimp pam-u2f rclone gparted 
 
 cd /tmp
 git clone https://aur.archlinux.org/package-query.git
@@ -13,18 +13,10 @@ git clone https://aur.archlinux.org/yaourt.git
 cd yaourt/
 makepkg -si
 
-yaourt -S pamac-all
-yaourt -S snapd
-yaourt -S timeshift
-
 cd /tmp 
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si 
-
-yay -S appimagelauncher
-
-yay -S icedrive-appimage
 
 cd/tmp
 git clone https://github.com/safing/portmaster-packaging
@@ -33,6 +25,43 @@ makepkg -is
 
 systemctl enable portmaster
 
-snap install dosbox-staging
+flatpak install com.wps.Office
+flatpak install org.gnome.clocks  
+flatpak install io.mgba.mgba
+flatpak install io.github.dosbox-staging
+
+yaourt -S pamac-all
+yaourt -S snapd
+yaourt -S timeshift
+yaourt -S timeshift-autosnap
+yaourt -S btrfs-autosnap
+yaourt -S chrome-gnome-shell
+yaourt -S debtap
+yaourt -S loginized
+yaourt -S masterpdfeditor-free
+yaourt -S mullvad-vpn
+yaourt -S virtualbox-ext-oracle
+yaourt -S gimp-plugin-gmic
+yaourt -S gimp-palletes-davidrevoy
+yaourt -S gimp-plugin-akkana-git
+yaourt -S gimp-plugin-astronomy 
+yaourt -S gimp-plugin-beautify
+yaourt -S gimp-plugin-contrastfix
+yaourt -S gimp-plugin-create-layer-mask-from
+yaourt -S gimp-plugin-duplicate-to-another-image
+yaourt -S gimp-plugin-export-layers
+yaourt -S gimp-plugin-image-reg
+yaourt -S gimp-plugin-instagram-effects
+yaourt -S gimp-plugin-layerfx
+yaourt -S gimp-plugin-layer-via-copy-cut
+yaourt -S gimp-plugin-pandora
+yaourt -S gimp-plugin-place-layer-into-selection
+yaourt -S gimp-plugin-registry
+yaourt -S gimp-plugin-scale-layer-to-image-size
+yaourt -S gimp-plugin-toy
+yaourt -S gimp-plugin-refocus
+
+yay -S appimagelauncher
+yay -S icedrive-appimage
 
 timedatectl status 
