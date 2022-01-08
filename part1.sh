f2ced6d94c8a17610a83a2238d424fecb1ea99f6
Eliliyah <94532192+Eliliyah@users.noreply.github.com>
2022-01-07 20:35:28 -0500

    Update part1.sh

--- part1.sh
+++ part1.sh
@@ -26,8 +26,8 @@ timedatectl set-ntp true
 sgdisk --zap-all /dev/nvme0n1
 
 sgdisk --clear \
-         --new=1:0:+550MiB --typecode=1:ef00 \
-         --new=2:0:+16GiB   --typecode=2:8200 \
+         --new=1:0:+3GiB --typecode=1:ef00 \
+         --new=2:0:+32GiB   --typecode=2:8200 \
          --new=3:0:0       --typecode=3:8300 \
            /dev/nvme0n1
 confirm "Did it zap?"
@@ -39,15 +39,16 @@ mkfs.btrfs /dev/nvme0n1p3 --label=system -f
 o=defaults,x-mount.mkdir
 o_btrfs=$o,defaults,noatime,autodefrag,compress=zstd
 mount -t btrfs LABEL=system /mnt 
-btrfs subvolume create /mnt/root
-btrfs subvolume create /mnt/home
-btrfs subvolume create /mnt/snapshots
+btrfs subvolume create /mnt/@
+btrfs subvolume create /mnt/@home
+btrfs subvolume create /mnt/@snapshots
 umount -R /mnt
 
 #Mount the partitions
-mount -t btrfs -o subvol=root,$o_btrfs LABEL=system /mnt
-mount -t btrfs -o subvol=home,$o_btrfs LABEL=system /mnt/home
-mount -t btrfs -o subvol=snapshots,$o_btrfs LABEL=system /mnt/snapshots
+mount -t btrfs -o subvol=@,$o_btrfs LABEL=system /mnt
+mount -t btrfs -o subvol=@home,$o_btrfs LABEL=system /mnt/home
+mkdir /mnt/.snapshots
+mount -t btrfs -o subvol=@snapshots,$o_btrfs LABEL=system /mnt/.snapshots
 mkdir /mnt/boot
 mkfs.fat -F 32 -n EFI /dev/nvme0n1p1 
 mount /dev/nvme0n1p1 /mnt/boot
@@ -58,7 +59,7 @@ confirm "Partitions look okay?"
 #Install the base system
 pacstrap /mnt base linux linux-firmware
 pacstrap /mnt btrfs-progs
-pacstrap /mnt dhcpcd linux-zen base-devel iw gptfdisk zsh terminus-font intel-ucode snapper grub dosfstools man-db man-pages nano usbutils util-linux which neofetch python efibootmgr efitools efivar reflector perl perl-timedate iwd git systemd grub-btrfs xorg xdg-user-dirs 
+pacstrap /mnt dhcpcd linux-zen linux-zen-headers base-devel iw gptfdisk zsh terminus-font intel-ucode snapper grub dosfstools man-db man-pages nano usbutils util-linux which neofetch python efibootmgr efitools efivar reflector perl perl-timedate iwd git systemd grub-btrfs xorg xdg-user-dirs 
 confirm "Did it work?"
 
 #Generate fstab
