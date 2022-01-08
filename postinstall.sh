0d5155c16b5dabad99ce65da5f5ae51050b34a18
Eliliyah <94532192+Eliliyah@users.noreply.github.com>
2022-01-07 20:37:23 -0500

    Update postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -1,14 +1,11 @@
 #!/usr/bin/bash
 
-sudo pacman -Syu 
-sudo pacman -S flatpak libappimage wget yajl pipewire --needed pipewire-pulse pipewire-alsa pipewire-media-session pipewire-jack --noconfirm
-systemctl --user enable pipewire pipewire-pulse pipewire-media-session
-sudo pacman -S gimp discord meld file-roller opera-ffmpeg-codecs bitwarden code inkscape virtualbox qbittorrent strawberry thunderbird totem bpytop firefox gimp pam-u2f rclone gparted --noconfirm
+sudo pacman -Syu --noconfirm
+sudo pacman -S gimp libreoffice-fresh discord meld file-roller opera-ffmpeg-codecs bitwarden cheese code inkscape virtualbox qbittorrent strawberry thunderbird totem bpytop firefox gimp pam-u2f rclone gparted go meson cmake extra-cmake-modules rust
+sudo pacman -S virtualbox-ext-vnc virtualbox-guest-iso virtualbox-guest-utils virtualbox-host-dkms virtualbox-sdk --noconfirm
 
-flatpak install flathub io.mgba.mGBA --noconfirm
-flatpak install flathub io.github.dosbox-staging --noconfirm
-flatpak install flathub org.gnome.clocks --noconfirm
-flatpak install flathub com.wps.Office --noconfirm
+
+flatpak install flathub mGBA dosbox-staging 
 
 cd /tmp
 git clone https://aur.archlinux.org/aura-bin.git
@@ -16,17 +13,12 @@ cd aura-bin
 makepkg -si
 sudo pacman -Syu
 
-sudo aura -A pipewire-jack-dropin
+sudo aura -A pipewire-jack-dropin snapper-support snap-pac --noconfirm
 sudo aura -A pamac-all --noconfirm
-sudo aura -A timeshift timeshift-autosnap btrfs-autosnap ch.protonmail.protonmail-bridge --noconfirm
-sudo aura -A chrome-gnome-shell debtap loginized masterpdfeditor-free mullvad-vpn virtualbox-ext-oracle --noconfirm
+sudo aura -A update-grub grub-hook btrfs-autosnap snap-pac-grub ch.protonmail.protonmail-bridge stacer-bin --noconfirm
+sudo aura -A chrome-gnome-shell debtap masterpdfeditor-free mullvad-vpn virtualbox-ext-oracle --noconfirm
 sudo aura -A appimagelauncher --noconfirm
-sudo aura -A -S icedrive-appimage --noconfirm
+sudo aura -A icedrive-appimage --noconfirm
+sudo aura -A hunspell-en-med-glut-git intel-cpu-runtime libreoffice-extension-cleandoc libreoffice-extension-languagetool libreoffice-extension-minicorrector ocs-url nerd-fonts-complete onevpl-intel-gpu pacdiff-pacman-hook-git qgnomeplatform systemd-boot-pacman-hook virtualbox-guest-goodies wd719x-firmware aic94xx-firmware 
+
 sudo aura -A gimp-plugin-gmic gimp-palletes-davidrevoy gimp-plugin-akkana-git gimp-plugin-astronomy gimp-plugin-beautify gimp-plugin-contrastfix gimp-plugin-create-layer-mask-from gimp-plugin-duplicate-to-another-image gimp-plugin-export-layers gimp-plugin-image-reg gimp-plugin-instagram-effects gimp-plugin-layerfx gimp-plugin-layer-via-copy-cut gimp-plugin-pandora gimp-plugin-place-layer-into-selection gimp-plugin-registry gimp-plugin-scale-layer-to-image-size gimp-plugin-toy gimp-plugin-refocus --noconfirm
-
-cd/tmp
-git clone https://github.com/safing/portmaster-packaging
-cd portmaster-packaging/linux
-makepkg -is
-
-systemctl enable portmaster
