51828bf711355238ef6f86f548e421c76b3f0494
Eliliyah <94532192+Eliliyah@users.noreply.github.com>
2022-01-09 02:07:45 -0500

    Update VM1.sh

--- VM1.sh
+++ VM1.sh
@@ -38,17 +38,17 @@ mkfs.btrfs /dev/sda3 --label=system -f
 o=defaults,x-mount.mkdir
 o_btrfs=$o,defaults,noatime,autodefrag,compress=zstd
 mount -t btrfs LABEL=system /mnt 
-btrfs subvolume create /mnt/
-btrfs subvolume create /mnt/@root
+btrfs subvolume create /mnt/@
 btrfs subvolume create /mnt/@home
-btrfs subvolume create /mnt/snapshots
+btrfs subvolume create /mnt/@.snapshots
+btrfs subvolume create /mnt/@var
 umount -R /mnt
 
 #Mount the partitions
-mount -t btrfs -o subvol=root,$o_btrfs LABEL=system /mnt
-mount -t btrfs -o subvol=home,$o_btrfs LABEL=system /mnt/@root
-mount -t btrfs -o subvol=home,$o_btrfs LABEL=system /mnt/@home
-mount -t btrfs -o subvol=snapshots,$o_btrfs LABEL=system /mnt/snapshots
+mount -t btrfs -o subvol=@,$o_btrfs LABEL=system /mnt
+mount -t btrfs -o subvol=@home,$o_btrfs LABEL=system /mnt/home
+mount -t btrfs -o subvol=@.snapshots,$o_btrfs LABEL=system /mnt/.snapshots
+mount -t btrfs -o subvol=@var,$o_btrfs LABEL=system /mnt/var
 mkdir /mnt/boot
 mkfs.fat -F 32 -n EFI /dev/sda1 
 mount /dev/sda1 /mnt/boot
