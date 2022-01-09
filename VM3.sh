438d18085ea7e35866d2705f97ca4f3b764785c0
Eliliyah <94532192+Eliliyah@users.noreply.github.com>
2022-01-09 02:13:39 -0500

    Update VM3.sh

--- VM3.sh
+++ VM3.sh
@@ -1,5 +1,7 @@
-sudo pacman -Syu 
-sudo pacman -S flatpak libappimage wget yajl
+#!/usr/bin/bash
+
+sudo pacman -Syu --noconfirm
+sudo pacman -S file-roller go meson cmake extra-cmake-modules rust
 
 cd /tmp
 git clone https://aur.archlinux.org/aura-bin.git
@@ -7,4 +9,9 @@ cd aura-bin
 makepkg -si
 sudo pacman -Syu
 
-sudo aura -A paperde
+sudo aura -A pipewire-jack-dropin snapper-support snap-pac --noconfirm
+sudo aura -A pamac-all --noconfirm
+sudo aura -A update-grub grub-hook btrfs-autosnap snap-pac-grub stacer-bin --noconfirm
+sudo aura -A appimagelauncher --noconfirm
+sudo aura -A icedrive-appimage --noconfirm
+sudo aura -A hunspell-en-med-glut-git ocs-url nerd-fonts-complete pacdiff-pacman-hook-git qgnomeplatform systemd-boot-pacman-hook wd719x-firmware aic94xx-firmware 
