6842c7b982ebf28ee46e7fffdc69d65c144ef962
Eliliyah <94532192+Eliliyah@users.noreply.github.com>
2022-01-10 05:47:54 -0500

    Update mount.sh

--- mount.sh
+++ mount.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/bash
-mount -t btrfs -o subvol=root,$o_btrfs LABEL=system /mnt
-mount -t btrfs -o subvol=home,$o_btrfs LABEL=system /mnt/home
-mount -t btrfs -o subvol=snapshots,$o_btrfs LABEL=system /mnt/snapshots
-mount /dev/sda1 /mnt/boot
+mount -t btrfs -o subvol=@,$o_btrfs LABEL=system /mnt
+mount -t btrfs -o subvol=@home,$o_btrfs LABEL=system /mnt/home
+mount -t btrfs -o subvol=@var,$o_btrfs LABEL=system /mnt/var
+mount -t btrfs -o subvol=@.snapshots,$o_btrfs LABEL=system /mnt/.snapshots
+mount /dev/nvme0n1p1 /mnt/boot
 swapon -L swap
 lsblk
