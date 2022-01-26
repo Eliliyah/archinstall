f739a749d4c9ff4522b8c0c349442a4eae10957b
Eliliyah <94532192+Eliliyah@users.noreply.github.com>
2022-01-26 13:50:31 -0500

    Update part2.sh

--- part2.sh
+++ part2.sh
@@ -113,6 +113,17 @@ aura -A pamac-all
 aura -A stacer-bin chromium-extension-plasma-integration intel-cpu-runtime onevpl-intel-gpu hunspell-en-med-glut-git --noconfirm 
 confirm "All good?" 
 
+#Install zram
+echo "zram">> /etc/modules-load.d/zram.conf
+echo "options zram num_devices=2">> /etc/modprobe.d/zram.conf
+echo "KERNEL=="zram0", ATTR{disksize}="512M" RUN="/usr/bin/mkswap /dev/zram0", TAG+="systemd"
+KERNEL=="zram1", ATTR{disksize}="512M" RUN="/usr/bin/mkswap /dev/zram1", TAG+="systemd"">> /etc/udev/rules.d/99-zram.rules
+echo "
+/dev/zram0 none swap defaults 0 0
+/dev/zram1 none swap defaults 0 0" >> /etc/fstab
+nano /etc/fstab
+confirm "Zram looking good?"
+
 #Install blackarch
 cd/tmp
 curl -O https://blackarch.org/strap.sh
