8ffbc159759386a2810f0d25d56e3e83ddd05ffc
Eliliyah <94532192+Eliliyah@users.noreply.github.com>
2022-03-06 03:11:07 -0500

    Update partitions.sh

--- partitions.sh
+++ partitions.sh
@@ -1,23 +1,7 @@
-#!/usr/bin/bash
 
-#FUNCTIONS GO HERE
-
-confirm() {         
-    while true; do
-        read -p "${1}" yn
-        case $yn in
-            [Yy]* ) $2; break;;
-            [Nn]* ) exit;;
-            * ) echo "Please answer Y or N.";;
-        esac
-    done
-}
-example-function() {
-    echo "$2"
-}
 #Partition the drive and create subvolumes
-mkfs.fat -F 32 -n EFI /dev/sda1
-mkswap -L swap -f /dev/sda2
+mkfs.fat -F 32 -n EFI /dev/sda1 
+mkswap -L swap -f /dev/sda2 
 mkfs.btrfs /dev/sda3 --label=system -f
 o=defaults,x-mount.mkdir
 o_btrfs=$o,defaults,noatime,autodefrag,compress=lzo
@@ -30,14 +14,19 @@ btrfs subvolume create /mnt/@cache
 btrfs subvolume create /mnt/@log
 btrfs subvolume create /mnt/@tmp
 btrfs subvolume create /mnt/@var
-mkdir /mnt/boot
-mkdir /mnt/@
-mkdir /mnt/@home
-mkdir /mnt/@root
-mkdir /mnt/@srv
-mkdir /mnt/@cache
-mkdir /mnt/@log
-mkdir /mnt/@tmp
-mkdir /mnt/@var
 umount -R /mnt
+
+#Mount the partitions
+mount -t btrfs -o subvol=@,$o_btrfs LABEL=system /mnt
+mount -t btrfs -o subvol=@home,$o_btrfs LABEL=system /mnt/home
+mount -t btrfs -o subvol=@root,$o_btrfs LABEL=system /mnt/root
+mount -t btrfs -o subvol=@srv,$o_btrfs LABEL=system /mnt/srv
+mount -t btrfs -o subvol=@cache,$o_btrfs LABEL=system /mnt/cache
+mount -t btrfs -o subvol=@log,$o_btrfs LABEL=system /mnt/log
+mount -t btrfs -o subvol=@tmp,$o_btrfs LABEL=system /mnt/tmp
+mount -t btrfs -o subvol=@var,$o_btrfs LABEL=system /mnt/var
+mkdir /mnt/boot
+mount /dev/sda1 /mnt/boot
+swapon -L swap
 lsblk
+confirm "Partitions look okay?" 
