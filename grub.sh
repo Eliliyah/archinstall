eef37fe2fb631a18cbcdf428ecd10d58f00bf9d4
Eliliyah <94532192+Eliliyah@users.noreply.github.com>
2022-03-06 19:17:08 -0500

    Update grub.sh

--- grub.sh
+++ grub.sh
@@ -17,13 +17,16 @@ example-function() {
 }
 
 #Install bootloader
-pacman -S grub grub-btrfs efibootmgr efivar efitools
-confirm "Did the packages install successfully?"
+pacman -S grub grub-btrfs efibootmgr efivar efitools --noconfirm
 grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
-ls /etc/default
-confirm "Is the grub file there?"
-echo "GRUB_THEME="/usr/share/grub/themes/EllieOS/theme.txt"">> /etc/default/grub
+sed -i '5,6 s/^/#/' /etc/default/grub
+echo "GRUB_DISTRIBUTOR="EllieOS"
+GRUB_CMDLINE_LINUX_DEFAULT="loglevel=3"
+GRUB_THEME="/usr/share/grub/themes/EllieOS/theme.txt"">> /etc/default/grub
+mv /archinstall/EllieOS /usr/share/grub/themes
+grub-mkconfig -o /boot/grub/grub.cfg
+confirm "Do you need to run it again?"
 nano /etc/default/grub
 mv /archinstall/EllieOS /usr/share/grub/themes
 grub-mkconfig -o /boot/grub/grub.cfg
