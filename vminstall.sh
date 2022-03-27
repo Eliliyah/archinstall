2d3152cc84ebeccac5dfe0a2218ee1fe1830f2c2
Eliliyah <94532192+Eliliyah@users.noreply.github.com>
2022-03-26 23:47:51 -0400

    Update vminstall.sh

--- vminstall.sh
+++ vminstall.sh
@@ -33,6 +33,6 @@ confirm "Was the base system installed?"
 echo "git clone https://github.com/Eliliyah/archinstall
 chmod +x /archinstall/arch-chroot.sh
 cd archinstall
-./arch-chroot.sh" >> /mnt/script.sh
+./vm-arch-chroot.sh" >> /mnt/script.sh
 chmod +x /mnt/script.sh
 arch-chroot /mnt /script.sh
