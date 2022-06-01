3c695b1c90088b1d06c492c3f78fa7e811025871
Eliliyah <94532192+Eliliyah@users.noreply.github.com>
2022-06-01 05:31:21 -0400

    Update chroot2.sh

--- chroot2.sh
+++ chroot2.sh
@@ -1,6 +1,5 @@
 #Mount the partitions
 mount -t btrfs -o subvol=@,$o_btrfs LABEL=system /mnt
-mount -t btrfs -o subvol=@/.snapshots,$o_btrfs LABEL=system /mnt/.snapshots
 mount -t btrfs -o subvol=@/home,$o_btrfs LABEL=system /mnt/home
 mount -t btrfs -o subvol=@/root,$o_btrfs LABEL=system /mnt/root
 mount -t btrfs -o subvol=@/srv,$o_btrfs LABEL=system /mnt/srv
