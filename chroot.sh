9d6de97910df33a2e0e48440b89eee6bd97fd2ba
EllieOS <94532192+Eliliyah@users.noreply.github.com>
2022-06-06 01:12:21 -0400

    Update chroot.sh

--- chroot.sh
+++ chroot.sh
@@ -18,13 +18,13 @@ example-function() {
 
 #Mount the partitions
 mount -t btrfs -o subvol=@,$o_btrfs LABEL=system /mnt
-mount -t btrfs -o subvol=@home,$o_btrfs LABEL=system /mnt/home
-mount -t btrfs -o subvol=@root,$o_btrfs LABEL=system /mnt/root
-mount -t btrfs -o subvol=@srv,$o_btrfs LABEL=system /mnt/srv
-mount -t btrfs -o subvol=@cache,$o_btrfs LABEL=system /mnt/cache
-mount -t btrfs -o subvol=@log,$o_btrfs LABEL=system /mnt/log
-mount -t btrfs -o subvol=@tmp,$o_btrfs LABEL=system /mnt/tmp
-mount -t btrfs -o subvol=@var,$o_btrfs LABEL=system /mnt/var
+mount -t btrfs -o subvol=@/home,$o_btrfs LABEL=system /mnt/home
+mount -t btrfs -o subvol=@/root,$o_btrfs LABEL=system /mnt/root
+mount -t btrfs -o subvol=@/srv,$o_btrfs LABEL=system /mnt/srv
+mount -t btrfs -o subvol=@/cache,$o_btrfs LABEL=system /mnt/cache
+mount -t btrfs -o subvol=@/log,$o_btrfs LABEL=system /mnt/log
+mount -t btrfs -o subvol=@/tmp,$o_btrfs LABEL=system /mnt/tmp
+mount -t btrfs -o subvol=@/var,$o_btrfs LABEL=system /mnt/var
 mount LABEL=EFI /mnt/boot
 swapon -L swap
 lsblk
