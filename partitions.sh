551ba2fcff4e8884676bfff1c641b42c95c7c0e3
EllieOS <94532192+Eliliyah@users.noreply.github.com>
2022-06-07 01:13:37 -0400

    Update partitions.sh

--- partitions.sh
+++ partitions.sh
@@ -28,13 +28,19 @@ mount -t btrfs LABEL=system /mnt
 #create subvolumes
 btrfs subvolume create /mnt/@
 mkdir /mnt/boot
-btrfs subvolume create /mnt/@/home
-btrfs subvolume create /mnt/@/root
-btrfs subvolume create /mnt/@/srv
-btrfs subvolume create /mnt/@/cache
-btrfs subvolume create /mnt/@/log
-btrfs subvolume create /mnt/@/tmp
-btrfs subvolume create /mnt/@/var
+btrfs subvolume create /mnt/@home
+btrfs subvolume create /mnt/@root
+btrfs subvolume create /mnt/@srv
+btrfs subvolume create /mnt/@var
+btrfs subvolume create /mnt/@log
+btrfs subvolume create /mnt/@tmp
+btrfs subvolume create /mnt/@cache
+rm -rf /mnt/log
+rm -rf /mnt/tmp
+rm -rf /mnt/cache
+mkdir /mnt/var/log
+mkdir /mnt/var/tmp
+mkdir /mnt/var/cache
 btrfs quota enable /mnt
 umount -R /mnt
 
@@ -44,10 +50,11 @@ mount -t btrfs -o subvol=@,$o_btrfs LABEL=system /mnt
 mount -t btrfs -o subvol=@/home,$o_btrfs LABEL=system /mnt/home
 mount -t btrfs -o subvol=@/root,$o_btrfs LABEL=system /mnt/root
 mount -t btrfs -o subvol=@/srv,$o_btrfs LABEL=system /mnt/srv
-mount -t btrfs -o subvol=@/cache,$o_btrfs LABEL=system /mnt/cache
-mount -t btrfs -o subvol=@/log,$o_btrfs LABEL=system /mnt/log
-mount -t btrfs -o subvol=@/tmp,$o_btrfs LABEL=system /mnt/tmp
 mount -t btrfs -o subvol=@/var,$o_btrfs LABEL=system /mnt/var
+mount -t btrfs -o subvol=@/log,$o_btrfs LABEL=system /mnt/var/log
+mount -t btrfs -o subvol=@/tmp,$o_btrfs LABEL=system /mnt/var/tmp
+mount -t btrfs -o subvol=@/cache,$o_btrfs LABEL=system /mnt/var/cache
 mount LABEL=EFI /mnt/boot
 swapon -L swap
+btrfs quota enable /mnt
 lsblk
