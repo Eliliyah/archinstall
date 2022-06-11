920ffeaff0107de6755296bc52e901f7dc304055
EllieOS <94532192+Eliliyah@users.noreply.github.com>
2022-06-11 01:08:05 -0400

    Update arch-chroot.sh

--- arch-chroot.sh
+++ arch-chroot.sh
@@ -24,8 +24,8 @@ chmod +x keyrings.sh
 ./keyrings.sh
 confirm "Did the keyrings install and mirrors update successfully?"
 
-chmod +x pacman.sh
-./pacman.sh
+chmod +x packages.sh
+./packages.sh
 confirm "Did all packages install successfully?"
 
 chmod +x config.sh
