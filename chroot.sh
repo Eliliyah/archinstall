9582f2faf8889306fe43bda6ae91619d490dc42b
EllieOS <94532192+Eliliyah@users.noreply.github.com>
2022-07-24 01:12:23 -0400

    Update chroot.sh

--- chroot.sh
+++ chroot.sh
@@ -17,15 +17,16 @@ example-function() {
 }
 
 #Mount the partitions
+o=defaults,x-mount.mkdir
+o_btrfs=$o,defaults,noatime,autodefrag,compress=lzo,discard=async,ssd
 mount -t btrfs -o subvol=@,$o_btrfs LABEL=system /mnt
-mount -t btrfs -o subvol=@/home,$o_btrfs LABEL=system /mnt/home
-mount -t btrfs -o subvol=@/root,$o_btrfs LABEL=system /mnt/root
-mount -t btrfs -o subvol=@/srv,$o_btrfs LABEL=system /mnt/srv
-mount -t btrfs -o subvol=@/cache,$o_btrfs LABEL=system /mnt/cache
-mount -t btrfs -o subvol=@/log,$o_btrfs LABEL=system /mnt/log
-mount -t btrfs -o subvol=@/tmp,$o_btrfs LABEL=system /mnt/tmp
-mount -t btrfs -o subvol=@/var,$o_btrfs LABEL=system /mnt/var
-mount LABEL=EFI /mnt/boot
-swapon -L swap
+mount -t btrfs -o subvol=@home,$o_btrfs LABEL=system /mnt/home
+mount -t btrfs -o subvol=@root,$o_btrfs LABEL=system /mnt/root
+mount -t btrfs -o subvol=@srv,$o_btrfs LABEL=system /mnt/srv
+mount -t btrfs -o subvol=@log,$o_btrfs LABEL=system /mnt/var/log
+mount -t btrfs -o subvol=@tmp,$o_btrfs LABEL=system /mnt/var/tmp
+mount -t btrfs -o subvol=@cache,$o_btrfs LABEL=system /mnt/var/cache
+mount /dev/nvme0n1p1 /mnt/boot
+swapon /dev/nvme0n1p2
+btrfs quota enable /mnt
 lsblk
-arch-chroot /mnt
